8e90da6e8d9b8b2c68fb519f2e227df82c3957dc
Juan Manuel Torres <juant@mindtouch.com>
2016-08-16 15:31:12 -0700

    Update emr code

--- airport_performance.py
+++ airport_performance.py
@@ -4,8 +4,8 @@ from urlparse import urlparse
 from pyspark import SparkContext
 import boto3
 
-#input_file = 's3://juant-test/emr-input/791696577_T_ONTIME_2015-01.csv'
-#output_file = 's3://juant-test/emr-output/san_diego_flights.csv'
+#input_file = 's3://team-TA-3-data-pipeline-magic-night/emr-input/791696577_T_ONTIME_2015-01.csv'
+#output_file = 's3://team-TA-3-data-pipeline-magic-night/emr-output/san_diego_flights.csv'
 local_results = '/tmp/san_diego_flights.csv'
 
 # Get arguments
@@ -32,8 +32,12 @@ header = data.first()
 rows = data.filter(lambda line: line != header)
 print('Filtering and collecting data')
 from_to_destinations = (rows.map(lambda line: line.split(','))
+                        # Select only the date, origin, destination and delay information
                         .map(lambda line: ('{}'.format(line[5]), '{},{}'.format(line[15], line[16]).replace('"', ''), '{},{}'.format(line[25], line[26]).replace('"', ''), '{}'.format(line[33])))
+                        # Include only San Diego as an origin
                         .filter(lambda line: True if line[1] == 'San Diego, CA' else False)
+                        # Do not include data that has no delay information
+                        .filter(lambda line: False if line[3] == '' else True)
                         .collect())
 
 # Generate a CSV file with the results of from_to_destinations
