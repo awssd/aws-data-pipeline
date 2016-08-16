8ec93300845b8f057945a0cccadfc8fe61625e8e
Juan Manuel Torres <juant@mindtouch.com>
2016-08-15 22:46:19 -0700

    Fist commit

---
+++ airport_performance.py
@@ -0,0 +1,52 @@
+import csv
+import sys
+from urlparse import urlparse
+from pyspark import SparkContext
+import boto3
+
+#input_file = 's3://juant-test/emr-input/791696577_T_ONTIME_2015-01.csv'
+#output_file = 's3://juant-test/emr-output/san_diego_flights.csv'
+local_results = '/tmp/san_diego_flights.csv'
+
+# Get arguments
+print('Number of arguments: {} arguments.'.format(len(sys.argv)))
+print('Argument List:' + str(sys.argv))
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# Parse output
+parts = urlparse(output_file)
+output_bucket = parts.netloc
+output_key = parts.path.lstrip('/')
+
+# Create a simple SparkContext
+sc = SparkContext(appName="SimpleApp")
+
+# load data from S3
+print('Loading data from "{}"'.format(input_file))
+data = sc.textFile(input_file)
+
+# Select the date, origin, destination and delay for all flights from San Diego, CA
+print('Removing header')
+header = data.first()
+rows = data.filter(lambda line: line != header)
+print('Filtering and collecting data')
+from_to_destinations = (rows.map(lambda line: line.split(','))
+                        .map(lambda line: ('{}'.format(line[5]), '{},{}'.format(line[15], line[16]).replace('"', ''), '{},{}'.format(line[25], line[26]).replace('"', ''), '{}'.format(line[33])))
+                        .filter(lambda line: True if line[1] == 'San Diego, CA' else False)
+                        .collect())
+
+# Generate a CSV file with the results of from_to_destinations
+print('Saving data as CSV to local file')
+with open(local_results, 'wb') as csv_file:
+    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    for line in from_to_destinations:
+        wr.writerow(line)
+
+# NEED TO FIND A DIFFERENT WAY TO UPLOAD FILE TO S3
+print('Uploading file to bucket: {} key: {}'.format(output_bucket, output_key))
+s3 = boto3.client('s3')
+with open(local_results, 'rb') as data:
+    s3.upload_fileobj(data, output_bucket, output_key)
+
+print('DONE')
